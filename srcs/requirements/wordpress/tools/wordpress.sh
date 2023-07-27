042c3c06193941df9b7e707c971fc791aa4083d1
kmendes- <kmendes-@Inception.myguest.virtualbox.org>
2023-07-27 10:42:31 +0200

    Continue work on wordpress container setup

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -8,12 +8,12 @@ if [! -f /var/www/html/wp-config.php]
 	cd /usr/local/bin
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-	mv wp-cli.phar wp
+	mv ./wp-cli.phar ./wp
 	cd /var/www/html
 	wp core download
-	wp core config --dbhost=<host.db> --dbname=<prefix_db> --dbuser=<username> --dbpass=<password>
-	wp core install --url=kmendes-.42.fr --title="Inception" --admin-name=<wordpress_admin> --admin_password=<password> --admin_email=<email>
-
+	wp core config --allow-root --dbhost=<host.db> --dbname=<prefix_db> --dbuser=<username> --dbpass=<password>
+	wp core install --allow-root --url=kmendes-.42.fr --title="Inception" --admin-name=kmendes- --admin_password=qwertyuiop --admin-email=kmendes-.student@42.fr
+	wp user create --allow-root random random@gmail.com --user_pass=test_pass
 fi
 
 echo "Finished Wordpress setup."
