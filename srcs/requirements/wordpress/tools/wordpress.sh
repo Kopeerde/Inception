1e4a2624b0f46a2f648a74e5e2bc969a190f23f4
Kopeerde <kevin.mendesg@gmail.com>
2023-10-12 13:08:18 +0200

    Fixing wordpress php errors and downloading wp-cli, next, fixing mariadb container

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -5,7 +5,7 @@ echo "Starting Wordpress."
 #chown -R www-data:www-data /var/www/*;
 chown -R 775 /var/www/*;
 mkdir -p /run/php;
-touch /run/php/php8.1-fpm.pid;
+touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	echo `php --version`
