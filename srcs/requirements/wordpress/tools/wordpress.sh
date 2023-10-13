a42612df38c3b5ca74b53bfeaee00ac11cd1ce99
Kopeerde <kevin.mendesg@gmail.com>
2023-10-13 17:54:22 +0200

    Fixing connection between nginx and worpress , WIP : Redo some setup to pass the install part of wordpress

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -2,7 +2,7 @@
 
 echo "Starting Wordpress."
 
-#sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "etc/php/7.4/pool.d/www.conf";
+sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "etc/php/7.4/fpm/pool.d/www.conf";
 chown -R www-data:www-data /var/www/*;
 chown -R 775 /var/www/*;
 mkdir -p /run/php/;
